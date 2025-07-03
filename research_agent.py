3bb2c0704393c75fc188ae8614773b3509928a75
imshayma <97676868+imshayma@users.noreply.github.com>
2025-07-03 15:24:48 +0400

    Update research_agent.py

--- research_agent.py
+++ research_agent.py
@@ -1,1 +1,18 @@
+import json
 
+class ResearchAgent:
+    def __init__(self):
+        with open("cafe_data.json", "r") as file:
+            self.data = json.load(file)
+
+    def get_ingredients(self, drink):
+        return self.data.get("ingredients", {}).get(drink.title(), "Not found")
+
+    def get_nutrition(self, item):
+        return self.data.get("nutrition", {}).get(item.title(), "Not found")
+
+    def get_hours(self, day):
+        return self.data.get("hours", {}).get(day.title(), "Not found")
+
+    def get_drink_list(self):
+        return list(self.data.get("ingredients", {}).keys())
